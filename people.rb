d09ee9ff0f2cb75b0ef1920712aabf215d313448
Demi Lee <demilee.to@gmail.com>
2017-09-28 14:06:27 -0400

    complete person exercise

--- people.rb
+++ people.rb
@@ -1,12 +1,36 @@
-class Student
+class Person
+  attr_accessor :name
 
-  def self.learn
-    puts "I get it!"
+  def initialize(name)
+    @name = name
   end
+
+  def greeting
+    puts "Hi, my name is #{@name}."
+  end
+
 end
 
-class Instructor
-  def self.teach
+class Student < Person
+
+  def learn
+    puts "I get it!"
+  end
+
+end
+
+class Instructor < Person
+  def teach
     puts "Everything in Ruby is an Object."
   end
 end
+
+nadia = Instructor.new("Nadia")
+chris = Student.new("Chris")
+nadia.greeting
+chris.greeting
+nadia.teach
+chris.learn
+# chris.teach
+### the above does not work because there is no teach method in either
+### the Student or Person class.
